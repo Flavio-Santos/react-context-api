2934796e78e5d7c79f987b0916df5e07a24eaef4
Flavio Santos Alves <flavio.santos.pro@gmail.com>
2026-02-01 15:29:56 -0300

    feat(03):get and post

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,18 @@
 import axios from 'axios';
+import { IUsuario } from '../types';
 
 const api = axios.create({
   baseURL: 'http://localhost:5000',
 });
 
-export default api;
+export const obterUsuario = async (id: number): Promise<IUsuario[]> => {
+  const { data } = await api.get<IUsuario[]>(`/usuarios`);
+  return data;
+};
+
+export const criarUsuario = async (
+  usuario: Omit<IUsuario, 'id'>,
+): Promise<IUsuario> => {
+  const { data } = await api.post<IUsuario>(`/usuarios`, usuario);
+  return data;
+};
